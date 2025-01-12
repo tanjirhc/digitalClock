99326471e8676ee3b556a8ddd9ebb820d054c12d
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-13 00:55:35 +0600

    Date State variable

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from "react";
 
 function DigitalClock(){
 
+  const [time, setTime] = useState(new Date());
+
   return(
     <div className="clock-container">
       <div className="clock">
